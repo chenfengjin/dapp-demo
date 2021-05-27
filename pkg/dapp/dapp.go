57f53cc12d2a832740e8baae4079373f3428d492
chenfengjin <1871653365@qq.coom>
2021-05-27 10:56:55 +0800

    Remove posttx in get result

--- pkg/dapp/dapp.go
+++ pkg/dapp/dapp.go
@@ -1,77 +1,78 @@
 package dapp
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/xuperchain/dapp-demo/pkg/config"
 	xuperaccount "github.com/xuperchain/xuper-sdk-go/account"
 	"github.com/xuperchain/xuper-sdk-go/contract"
-	"net/http"
 )
 
 const (
-	GET_LUCK_ID="getLuckid"
-	START_LUCK_DRAW="startLuckDraw"
-	GET_RESULT = "getResult"
+	GET_LUCK_ID     = "getLuckid"
+	START_LUCK_DRAW = "startLuckDraw"
+	GET_RESULT      = "getResult"
 )
+
 var (
 	account *xuperaccount.Account
 )
 
-func init(){
+func init() {
 	var err error
-	account,err = xuperaccount.GetAccountFromPlainFile(config.KeyPath);
+	account, err = xuperaccount.GetAccountFromPlainFile(config.KeyPath)
 
-	if err!=nil{
+	if err != nil {
 		panic(err)
 	}
 }
 
-
-func Deploy(c*gin.Context){
-	args:= &struct {
+func Deploy(c *gin.Context) {
+	args := &struct {
 		ContractId string `json:"contract_id"`
 	}{}
 
-	if err:=c.BindJSON(args);err!=nil{
+	if err := c.BindJSON(args); err != nil {
 		c.Error(err)
 	}
-	if args.ContractId==""{
-		c.JSON(http.StatusBadRequest,"missing contract_id")
+	if args.ContractId == "" {
+		c.JSON(http.StatusBadRequest, "missing contract_id")
 		return
 	}
 	client := contract.InitWasmContract(
 		account,
-		config.Host ,
+		config.Host,
 		config.BCName,
 		args.ContractId,
 		config.ContractAccount,
-		)
+	)
 
-	TxId,err:=client.DeployWasmContract(
-		map[string]string{"admin":account.Address},
+	TxId, err := client.DeployWasmContract(
+		map[string]string{"admin": account.Address},
 		config.CodePath,
 		"c",
-		)
-	if err!=nil{
+	)
+	if err != nil {
 		c.Error(err)
 		return
 	}
-	c.JSON(http.StatusOK,gin.H{
-		"txid":TxId,
+	c.JSON(http.StatusOK, gin.H{
+		"txid": TxId,
 	})
 
 }
-func GetLuckId(c*gin.Context){
+func GetLuckId(c *gin.Context) {
 
-	args:= &struct {
+	args := &struct {
 		ContractId string `json:"contract_id"`
 	}{}
 
-	if err:=c.BindJSON(args);err!=nil{
+	if err := c.BindJSON(args); err != nil {
 		c.Error(err)
 	}
-	if args.ContractId==""{
-		c.JSON(http.StatusBadRequest,"missing contract_id")
+	if args.ContractId == "" {
+		c.JSON(http.StatusBadRequest, "missing contract_id")
 	}
 
 	client := contract.InitWasmContract(
@@ -80,83 +81,76 @@ func GetLuckId(c*gin.Context){
 		config.BCName,
 		args.ContractId,
 		config.ContractAccount,
-		)
+	)
 
 	preInvokeResp, err := client.PreInvokeWasmContract(GET_LUCK_ID, map[string]string{})
-	if err!=nil{
+	if err != nil {
 		c.Error(err)
 		return
 	}
-	_, err=client.PostWasmContract(preInvokeResp)
-	if err!=nil{
+	_, err = client.PostWasmContract(preInvokeResp)
+	if err != nil {
 		c.Error(err)
 		return
 	}
 
-	c.JSON(http.StatusOK,gin.H{
-		"data":map[string]string{
-			"id":string(preInvokeResp.GetResponse().GetResponse()[0]),
+	c.JSON(http.StatusOK, gin.H{
+		"data": map[string]string{
+			"id": string(preInvokeResp.GetResponse().GetResponse()[0]),
 		},
 	})
 }
-func StartLuckDraw(c*gin.Context){
-	args:= &struct {
+func StartLuckDraw(c *gin.Context) {
+	args := &struct {
 		ContractId string `json:"contract_id"`
-		Seed string `json:"seed"`
+		Seed       string `json:"seed"`
 	}{}
 	c.BindJSON(args)
-	client := contract.InitWasmContract(account,config.Host	,config.BCName,args.ContractId,"XC1111111111111111@xuper")
-	preInvokeResp,err:= client.PreInvokeWasmContract(START_LUCK_DRAW,map[string]string{
-		"seed":args.Seed,
+	client := contract.InitWasmContract(account, config.Host, config.BCName, args.ContractId, "XC1111111111111111@xuper")
+	preInvokeResp, err := client.PreInvokeWasmContract(START_LUCK_DRAW, map[string]string{
+		"seed": args.Seed,
 	})
-	if err!=nil{
+	if err != nil {
 		c.Error(err)
 		return
 	}
-	luckUser :=string(preInvokeResp.GetResponse().GetResponse()[0])
-	_,err =client.PostWasmContract(preInvokeResp)
-	if err!=nil{
+	luckUser := string(preInvokeResp.GetResponse().GetResponse()[0])
+	_, err = client.PostWasmContract(preInvokeResp)
+	if err != nil {
 		c.Error(err)
 		return
 	}
-	c.JSON(http.StatusOK,gin.H{
-		"data":map[string]string{
-			"luck_user":luckUser,
+	c.JSON(http.StatusOK, gin.H{
+		"data": map[string]string{
+			"luck_user": luckUser,
 		},
 	})
 }
 
-func GetResult(c*gin.Context){
+func GetResult(c *gin.Context) {
 
-	args:= &struct {
+	args := &struct {
 		ContractId string `json:"contract_id"`
 	}{}
 
-	if err:=c.BindJSON(args);err!=nil{
+	if err := c.BindJSON(args); err != nil {
 		c.Error(err)
 	}
-	if args.ContractId==""{
-		c.JSON(http.StatusBadRequest,"missing contract_id")
+	if args.ContractId == "" {
+		c.JSON(http.StatusBadRequest, "missing contract_id")
 	}
 
-	client := contract.InitWasmContract(account,config.Host,"xuper",args.ContractId,"XC1111111111111111@xuper")
+	client := contract.InitWasmContract(account, config.Host, "xuper", args.ContractId, "XC1111111111111111@xuper")
 	preInvokeResp, err := client.PreInvokeWasmContract(GET_RESULT, map[string]string{})
-	if err!=nil{
+	if err != nil {
 		c.Error(err)
 		return
 	}
 
-	_, err=client.PostWasmContract(preInvokeResp)
-	if err!=nil{
-		c.Error(err)
-		return
-	}
-
-	c.JSON(http.StatusOK,gin.H{
-		"data":map[string]string{
-			"luck_user":string(preInvokeResp.GetResponse().GetResponse()[0]),
+	c.JSON(http.StatusOK, gin.H{
+		"data": map[string]string{
+			"luck_user": string(preInvokeResp.GetResponse().GetResponse()[0]),
 		},
 	})
 
 }
-
